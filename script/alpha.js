cf061a48d2309d14791bfd70363489da8f1146e1
deveshbis <deveshbiswas.cse@gmail.com>
2024-02-15 09:56:48 +0600

    Update Score And Life

--- script/alpha.js
+++ script/alpha.js
@@ -8,6 +8,38 @@
 
     
 // }
+function handleKeyboardKeyUpEvents(event){
+    const playerPressed = event.key;
+    
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if(playerPressed === expectedAlphabet){
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const cuurentScore = parseInt(currentScoreText);
+
+        const newScore = cuurentScore + 1;
+
+        currentScoreElement.innerText = newScore;
+        continueGame();
+        removeBackgroundColorById(expectedAlphabet);
+    }
+    else{
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+
+        currentLifeElement.innerText = newLife;
+    }
+
+}
+document.addEventListener('keyup',handleKeyboardKeyUpEvents)
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
--- script/utility.js
+++ script/utility.js
@@ -14,6 +14,10 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-orange-400');
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxy';
     const alphabets = alphabetString.split('');
